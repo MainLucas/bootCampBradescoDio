f7d82020b9c99e1d431ab91d17ee40cf62f05c0a
Lucas <mainn.lucas@gmail.com>
2025-02-12 12:44:52 -0300

    Modulo 01 - Anatomia de Classes aula 01 e 02

---
+++ JAVA-ANATOMIA-CLASSES/src/AMinhaClasse.java
@@ -0,0 +1,7 @@
+public class AMinhaClasse{
+    public static void main(String[] args){
+        final String BR = "Brasil"; // variavel com letra maiuscula como convensão o valor recebido não deve ser alterado.
+        String teste_Simbolos$Permitidos = "_$"; // unicos caracteres especiais permitidos para declarar o nome de uma variável são: _ e $
+
+    }
+}
