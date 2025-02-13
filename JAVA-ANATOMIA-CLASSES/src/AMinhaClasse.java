871a8cc0b7d1e566cae7f8bf9122b1e26b8975b7
Lucas <mainn.lucas@gmail.com>
2025-02-13 20:46:14 -0300

    Revert "docs: Adicionei nova aula sobre package e métodos"
    This reverts commit b7d8c9746ed4d887f8a9bdf5870b8421bb44a1f2.

--- JAVA-ANATOMIA-CLASSES/src/AMinhaClasse.java
+++ JAVA-ANATOMIA-CLASSES/src/AMinhaClasse.java
@@ -1,8 +1,7 @@
-package edu.devlucas.modulo01;
 public class AMinhaClasse{
     public static void main(String[] args){
         final String BR = "Brasil"; // variavel com letra maiuscula como convensão o valor recebido não deve ser alterado.
         String teste_Simbolos$Permitidos = "_$"; // unicos caracteres especiais permitidos para declarar o nome de uma variável são: _ e $
-            
+
     }
 }
